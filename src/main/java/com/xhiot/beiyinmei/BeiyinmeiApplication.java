8bcf7980e6e79165e98fabccd421a1d579498dcb
liuyq <2568835102@qq.com>
2020-09-11 09:21:41 +0800

    first commmit

---
+++ src/main/java/com/xhiot/beiyinmei/BeiyinmeiApplication.java
@@ -0,0 +1,13 @@
+package com.xhiot.beiyinmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiyinmeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeiyinmeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhiot/beiyinmei/BeiyinmeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhiot.beiyinmei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeiyinmeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
